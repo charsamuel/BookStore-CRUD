8d9af1fedb160918e01e72c7d52837563e705e8b
Charity Samuel <135695024+charsamuel@users.noreply.github.com>
2024-05-29 23:07:12 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-  "mongodb+srv://readers:gfafg3jJop40MRVn@book-store.sjvsazs.mongodb.net/?retryWrites=true&w=majority&appName=book-store";
+export const mongoDBURL =""
+  
